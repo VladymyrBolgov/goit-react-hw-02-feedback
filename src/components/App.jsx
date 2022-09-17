61c1fb304130152f47bc9217bcc613d69ba3c1d9
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-09-17 20:35:28 +0300

    del

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,45 +1,4 @@
-import { Component } from "react";
-import FeedbackOptions from "./FeedbackOptions";
-import Section from "./Section";
-import Statistics from "./Statistics";
-import { AppContainer, P, Div } from "./App.styled";
 
-export class App extends Component{
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-
-  
-
-  render() {
-    return (
-      <AppContainer>
-        <Div>
-          <Section title="Please leave feedback">
-            <FeedbackOptions
-              options={this.state}
-              onLeaveFeedback={this.onLeaveFeedback}
-              />
-          </Section>
-
-          <Section title="Statistics">
-            
-            <Statistics
-              good={this.state.Good}
-              neutral={this.state.Neutral}
-              bad={this.state.Bad}
-              total={this.handleTotal()}
-              positivePercentage={this.handlePosFeedbacks()} 
-            />
-            <P>No feedback given</P>
-          </Section>
-        </Div>
-      </AppContainer>
-    )
-  }
-}
 
 
 
