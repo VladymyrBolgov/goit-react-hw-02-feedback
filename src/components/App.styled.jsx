602ac7fc3d5d6ded73f6ef2bd241cd7d91847bed
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-09-17 20:50:37 +0300

    вернул

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -1,1 +1,20 @@
+import styled from "@emotion/styled";
 
+export const AppContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 40;
+  color: #010101;
+`;
+
+export const P = styled.p`
+  margin: 5px;
+`;
+
+export const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  box-shadow: 1px 2px 9px #f4aab9;
+`;
